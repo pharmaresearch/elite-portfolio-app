05e076f0eacedc9fc9a46dae189a794a8fb785b0
pharmaresearch <pharmaresearch87@gmail.com>
2026-04-03 10:09:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(url, data={"5522893777": CHAT_ID, "text": message})
+    requests.post(url, data={"chat_id": CHAT_ID, "text": message})
 
 print("🚀 Running Automated Elite Engine...\n")
 
